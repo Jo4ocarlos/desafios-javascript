deb1e417e49a4dbe232210f5114b922243807fe0
João Carlos Almeida <juke4892@gmail.com>
2024-06-10 10:39:35 -0300

    alterações no código

--- Projeto-Tabuada/script.js
+++ Projeto-Tabuada/script.js
@@ -1,62 +1,70 @@
-let inputInv = document.getElementById('limptab')
-let titulo = document.getElementById('titulo')
-let res = document.getElementById('res')
-let numtab = document.getElementById('txtn')
+// Selecionar elementos
+const multiplicationForm = document.querySelector("#multiplication-form");
+const numberInput = document.querySelector("#number");
+const multiplicationInput = document.querySelector("#multiplicator");
+const multiplicationTitle = document.querySelector("#multiplication-title span");
+const fatherTable = document.querySelector("#multiplication-operations");
+const cleanBtn = document.querySelector(".clean_btn");
 
-function clicar(){
-   
-    
-    if(numtab.value.length == 0|| numtab.value <0 ||numtab.value > 100){
-        alert('[ERRO]Digite um número de 0 a 100')
-    
-    }else{
-        let n1 = Number(numtab.value)
-        //(res.innerHTML='')Usei para não gerar uma tabuada embaixo da outra, quando gerar nova vai apagar a anterior.
-        res.innerHTML=''
-       
-        //título da tabuada
-        titulo.innerHTML = `Tabuada do ${n1}`
-        
-        
-        for(let c = 0; c<=10; c++){
-            // enquanto c for menor ou igual 10, c recebe 1
-          
-            res.innerHTML += `${n1}x${c}=${n1*c}<br>`
-           // código responsável por fazer os cálculos e mostrar na tela
-           
-           if(n1%2==0){
-            // se o número digitado for PAR fica azul se for ÍMPAR fica amarelo(estilização da tabuada)
-            res.style.background = 'aqua'
-           }else{
-            res.style.background= 'yellow '
-           }
-    
-        } 
-        inputInv.style.display = ' block'
-        res.style.display= 'block'
-        titulo.style.display= 'block'
-        //quando clicar mostrar conteúdos que escondi com o css
-        
-       
+// Funções 
+
+// Função responsável por fazer as operações e gerar o resultado
+const operation = (multiplication, multiplicator) => {
+    // Limpar a tabela existente
+    fatherTable.innerHTML = "";
+
+    for (let i = 0; i <= multiplicator; i++) {
+        const resultado = multiplication * i;
+        const resultText = `${multiplication} x ${i} = ${resultado}`;
+        createTable(resultText);
     }
-    numtab.value =''
-    numtab.focus()
-     // para limpar o imput e o cursor aparecer para nova digitação 
+};
 
-}
-// função para limpar tabuada e esconder novamente os conteúdos
-function limpar(){
-    
-    res.innerHTML=''
-    inputInv.style.display = 'none'
-    res.style.display= 'none'
-    titulo.style.display= 'none'
-    
-    numtab.value =''
-    numtab.focus()
-     // para limpar o imput e o cursor aparecer para nova digitação 
-  
-}
+// Função responsável por criar tabela e apresentar o resultado
+const createTable = result => {
+    const divTable = document.createElement("div");
+    divTable.classList.add("row");
+    divTable.textContent = result;
+    fatherTable.appendChild(divTable);
+};
+
+// Eventos
+
+// Evento responsável por fazer as validações e chamar a função 
+multiplicationForm.addEventListener("submit", event => {
+    event.preventDefault();
+
+    const multiplicationNumber = Number(numberInput.value);
+    const multiplicatorNumber = Number(multiplicationInput.value);
+
+    if (multiplicatorNumber < 0 || multiplicatorNumber > 100) {
+        alert("Digite um número de 0 a 100");
+        return;
+    }
+
+    if (!multiplicationNumber || !multiplicatorNumber) {
+        alert("Por favor, digite um número");
+    } else {
+        operation(multiplicationNumber, multiplicatorNumber);
+        multiplicationTitle.textContent = `${multiplicationNumber}`;
+
+        // Limpar os campos de entrada
+        numberInput.value = "";
+        multiplicationInput.value = "";
+    }
+});
+
+// Responsável por limpar a tabuada
+cleanBtn.addEventListener("click", event => {
+    event.preventDefault();
+
+    // Limpar os campos e tabuada atual
+    numberInput.value = "";
+    multiplicationInput.value = "";
+    fatherTable.innerHTML = "Informe um número para calcular uma tabuada...";
+    multiplicationTitle.textContent = "";
+});
+
 
 
 
